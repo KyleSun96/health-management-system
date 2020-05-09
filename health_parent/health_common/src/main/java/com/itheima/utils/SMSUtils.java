880e41ee4925ddbc60bc2833d23b6111e4e72914
KyleSun <swy0907163@163.com>
2020-05-09 21:43:17 +0800

    移除一些数据

--- health_parent/health_common/src/main/java/com/itheima/utils/SMSUtils.java
+++ health_parent/health_common/src/main/java/com/itheima/utils/SMSUtils.java
@@ -31,8 +31,8 @@ public class SMSUtils {
         final String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
         // 替换成你的AK
-        final String accessKeyId = "LTAI4G6pLANP8DfqkVzzLMwJ";// 你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "0tnxVbWvleHcm17klnhE9egAd99rTF";// 你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "你的accessKeyId";// 你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "你的accessKeySecret";// 你的accessKeySecret，参考本文档步骤2
         // 初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
         DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
